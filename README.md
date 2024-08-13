365ed2a904fdb99f9d5abd385ab7773fb1d33a35
woximorks <aaron.wood@moxiworks.com>
2024-08-13 00:17:28 -0700

    Show is public

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # README
 
-Seeds file updated to include Products
-Dummy user added
-Included UI info on the home view
+Show is public
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: [:show]
     def index
         @endpoints = Endpoint.all
         @attrs = Attr.all
         @associated_attrs = AssociatedAttr.all
+    end
+    def show
+        @endpoints = Endpoint.all
+        @attrs = Attr.all
     end
 end
