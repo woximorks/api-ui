40e3dcdd400a5601e7b3ee82ca0d320ad928cb22
woximorks <aaron.wood@moxiworks.com>
2024-08-20 15:41:37 -0700

    Associating endpoints and attributes using some logic added to the seed files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,53 @@
+#Endpoints
+endpoints = [
+    "ActionLog",
+    "Agent",
+    "Brand",
+    "BuyerTransaction",
+    "Company",
+    "Contact",
+    "EmailCampaign",
+    "Event",
+    "Gallery",
+    "Group",
+    "LeadSource",
+    "Listing",
+    "Office",
+    "PresentationLog",
+    "SellerTransaction",
+    "SoldListing",
+    "Task",
+    "Team",
+    "WebUser"
+]
+
+endpoints.each do |endpoint_title|
+    Endpoint.find_or_create_by!(endpoint_title: endpoint_title)
+end
+
+#Products
+products = [
+    "test"
+=begin
+    "ActivePipe",
+    "Roster",
+    "Engage",
+    "Websites",
+    "Promote",
+    "Present",
+    "Impress",
+    "Balance",
+    "Stellar",
+    "API",
+    "Email",
+    "Syndication"
+=end
+    ]
+
+products.each do |product_title|
+    Product.find_or_create_by!(product_title: product_title)
+end
+
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
     load seed
 end
@@ -7,3 +57,4 @@ user.update!(
     password: "password",
     password_confirmation: "password"
 )
+
