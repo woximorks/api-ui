1a7ae2f2af2952731362100de70d2c2113aae43e
woximorks <aaron.wood@moxiworks.com>
2024-09-23 12:25:01 -0700

    Working on Agent update statments now

--- db/agent.sql
+++ db/agent.sql
@@ -1,7 +1,4 @@
--- Request
- -- Create Agent array within request_type
- -- Include Show Request within request_type -> Agent array created
-
+/*
 UPDATE associated_attrs
 SET 
   ui_info = jsonb_set(
@@ -27,7 +24,7 @@ SET
       )
     ),
     '{Agent}', 
-    -- Ensure new agents are added without duplicating existing ones
+    -- Ensure new UI associations are added without duplicating existing ones
     (
       SELECT jsonb_agg(DISTINCT value)
       FROM jsonb_array_elements_text(
@@ -49,82 +46,65 @@ SET
   updated_at = CURRENT_TIMESTAMP
 WHERE attr_title = 'agent_uuid';
 
+*/
 
--- Update to include "Agent" in Endpoints Array 
--- Include the Agent -> ['API', 'Roster'] Array within ui_info
-WHERE attr_title = 'moxi_works_agent_id';
-
-WHERE attr_title = 'source_agent_id';
-WHERE attr_title = 'moxi_works_company_id';
-WHERE attr_title = 'parent_company_id';
-WHERE attr_title = 'include_access_level';
+UPDATE associated_attrs
+SET 
+  ui_info = jsonb_set(
+    jsonb_set(
+      jsonb_set(
+        jsonb_set(
+          ui_info, 
+          '{APIText}', 
+          -- Append new text to the existing 'APIText' or create it if it doesn't exist
+          to_jsonb((COALESCE(ui_info->>'APIText', '') || 'To include access level information for the agent in the response, pass true.'))
+        ), 
+        '{EngageText}', 
+          -- Append new text to the existing 'RosterText2' or create it if it doesn't exist
+          to_jsonb((COALESCE(ui_info->>'RosterText', '') || 'The Gross Commission Income goal, as seen throughout the Engage UI.'))
+      ), 
+      '{Products}', 
+      -- Ensure new products are added without duplicating existing ones
+      (
+        SELECT jsonb_agg(DISTINCT value)
+        FROM jsonb_array_elements_text(
+          COALESCE(ui_info->'Products', '[]'::jsonb) || '["Engage"]'::jsonb
+        )
+      )
+    ),
+    '{Agent}', 
+    -- Ensure new UI associations are added without duplicating existing ones
+    (
+      SELECT jsonb_agg(DISTINCT value)
+      FROM jsonb_array_elements_text(
+        COALESCE(ui_info->'Agent', '[]'::jsonb) || '["Engage"]'::jsonb
+      )
+    )
+  ),
+  associated_endpoint = jsonb_set(
+    associated_endpoint,
+    '{Endpoints}',
+    '["Agent"]'
+  ),
+  updated_at = CURRENT_TIMESTAMP
 WHERE attr_title = 'include_gci_goals';
-WHERE attr_title = 'include_partners';
-WHERE attr_title = 'include_reviews';
 
--- Response
-WHERE attr_title = 'moxi_works_agent_id';
-WHERE attr_title = 'client_agent_id';
-WHERE attr_title = 'mls_agent_id';
-WHERE attr_title = 'license';
-WHERE attr_title = 'mls_name';
-WHERE attr_title = 'mls_abbreviation';
-WHERE attr_title = 'agent_id';
-WHERE attr_title = 'moxi_works_office_id';
-WHERE attr_title = 'office_id';
-WHERE attr_title = 'client_office_id';
-WHERE attr_title = 'company_id';
-WHERE attr_title = 'client_company_id';
-WHERE attr_title = 'office_address_street';
-WHERE attr_title = 'office_address_street2';
-WHERE attr_title = 'office_address_city';
-WHERE attr_title = 'office_address_state';
-WHERE attr_title = 'office_address_zip';
-WHERE attr_title = 'office_mailing_address_street';
-WHERE attr_title = 'office_mailing_address_street2';
-WHERE attr_title = 'office_mailing_address_city';
-WHERE attr_title = 'office_mailing_address_state';
-WHERE attr_title = 'office_mailing_address_zip';
-WHERE attr_title = 'name';
-WHERE attr_title = 'first_name';
-WHERE attr_title = 'last_name';
-WHERE attr_title = 'nickname';
-WHERE attr_title = 'main_phone_number';
-WHERE attr_title = 'mobile_phone_number';
-WHERE attr_title = 'alt_phone_number';
-WHERE attr_title = 'office_phone_number';
-WHERE attr_title = 'primary_email_address';
-WHERE attr_title = 'secondary_email_address';
-WHERE attr_title = 'email_addresses';
-WHERE attr_title = 'lead_routing_email_address';
-WHERE attr_title = 'title';
-WHERE attr_title = 'bio';
-WHERE attr_title = 'designations';
-WHERE attr_title = 'uuid';
-WHERE attr_title = 'has_product_access';
-WHERE attr_title = 'has_engage_access';
-WHERE attr_title = 'access_level';
-WHERE attr_title = 'view_level';
-WHERE attr_title = 'teams';
-WHERE attr_title = 'website_base_url';
-WHERE attr_title = 'twitter';
-WHERE attr_title = 'google_plus';
-WHERE attr_title = 'facebook';
-WHERE attr_title = 'home_page';
-WHERE attr_title = 'profile_image_url';
-WHERE attr_title = 'profile_thumb_url';
-WHERE attr_title = 'region';
-WHERE attr_title = 'gci_goal';
-WHERE attr_title = 'buyer_commission_rate';
-WHERE attr_title = 'seller_commission_rate';
-WHERE attr_title = 'service_area_zip_codes';
-WHERE attr_title = 'agent_website';
-WHERE attr_title = 'alternate_offices';
-WHERE attr_title = 'available_mls';
-WHERE attr_title = 'partners';
-WHERE attr_title = 'company_programs';
-WHERE attr_title = 'source_metadata';
-WHERE attr_title = 'created_timestamp';
-WHERE attr_title = 'deactivated_timestamp';
-WHERE attr_title = 'profile_visible_online';
-WHERE attr_title = 'user_reviews';
+UPDATE associated_attrs
+SET
+  request_type = jsonb_set(
+    request_type,
+    '{Agent}',  -- Specify the path within the JSONB object
+    (COALESCE(request_type->'Agent', '[]'::jsonb) || '["Create Request"]'::jsonb)
+  ),
+  updated_at = CURRENT_TIMESTAMP
+WHERE attr_title IN (
+  'agent_uuid',
+  'moxi_works_agent_id',
+  'source_agent_id',
+  'moxi_works_company_id',
+  'parent_company_id',
+  'include_access_level',
+  'include_gci_goals',
+  'include_partners',
+  'include_reviews'
+);
