6736a2d5dbf0d9e8c14bd054c4ba1ba896f31e4a
woximorks <aaron.wood@moxiworks.com>
2024-09-26 23:35:12 -0700

    Company complete

--- db/company.sql
+++ db/company.sql
@@ -206,7 +206,7 @@ SET
   request_type = jsonb_set(
     request_type,
     '{Company}',
-    (COALESCE(request_type->'Company', '[]'::jsonb) || '["Show Response", "]'::jsonb)
+    (COALESCE(request_type->'Company', '[]'::jsonb) || '["Show Response"]'::jsonb)
   ),
   updated_at = CURRENT_TIMESTAMP
 WHERE attr_title in (
@@ -223,7 +223,7 @@ SET
   request_type = jsonb_set(
     request_type,
     '{Company}',
-    (COALESCE(request_type->'Company', '[]'::jsonb) || '["Index Request"]'::jsonb)
+    (COALESCE(request_type->'Company', '[]'::jsonb) || '["Index Response"]'::jsonb)
   ),
   updated_at = CURRENT_TIMESTAMP
 WHERE attr_title in (
@@ -234,5 +234,158 @@ WHERE attr_title in (
     'public_partner_attrs'
 );
 
-END $$;
+UPDATE associated_attrs 
+SET 
+  ui_info = jsonb_set(
+      jsonb_set(
+        jsonb_set(
+          ui_info, -- The corresponding column name of the field to be updated
+        '{APIText}', -- To add text information about the attribute and how it associates to a product
+          to_jsonb((COALESCE(ui_info->>'APIText', '') || 'Number of companies records to return per page.'))
+      ),
+      '{Products}',
+      (
+        SELECT jsonb_agg(DISTINCT value)
+        FROM jsonb_array_elements_text(
+          COALESCE(ui_info->'Products', '[]'::jsonb) || '["API"]'::jsonb -- The product name, ie API, Roster
+        )
+      )
+    ),
+    '{Company}', -- Set Product associations to the attribute association on a local (endpoint specific) level
+    (
+      SELECT jsonb_agg(DISTINCT value)
+      FROM jsonb_array_elements_text(
+        COALESCE(ui_info->'Company', '[]'::jsonb) || '["API"]'::jsonb -- Endpoint name, Product name, ie API, Roster
+      )
+    )
+  ),
+  associated_endpoints = jsonb_set(
+    associated_endpoints,
+    '{Endpoints}',
+    '["Company"]' -- Set attribute association to the endpoint on a global level
+  ),
+  updated_at = CURRENT_TIMESTAMP
+WHERE attr_title = 'per_page';
 
+UPDATE associated_attrs 
+SET 
+  ui_info = jsonb_set(
+      jsonb_set(
+        jsonb_set(
+          ui_info, -- The corresponding column name of the field to be updated
+        '{APIText}', -- To add text information about the attribute and how it associates to a product
+          to_jsonb((COALESCE(ui_info->>'APIText', '') || 'Page of companies records to return.'))
+      ),
+      '{Products}',
+      (
+        SELECT jsonb_agg(DISTINCT value)
+        FROM jsonb_array_elements_text(
+          COALESCE(ui_info->'Products', '[]'::jsonb) || '["API"]'::jsonb -- The product name, ie API, Roster
+        )
+      )
+    ),
+    '{Company}', -- Set Product associations to the attribute association on a local (endpoint specific) level
+    (
+      SELECT jsonb_agg(DISTINCT value)
+      FROM jsonb_array_elements_text(
+        COALESCE(ui_info->'Company', '[]'::jsonb) || '["API"]'::jsonb -- Endpoint name, Product name, ie API, Roster
+      )
+    )
+  ),
+  associated_endpoints = jsonb_set(
+    associated_endpoints,
+    '{Endpoints}',
+    '["Company"]' -- Set attribute association to the endpoint on a global level
+  ),
+  updated_at = CURRENT_TIMESTAMP
+WHERE attr_title = 'page_number';
+
+UPDATE associated_attrs 
+SET 
+  ui_info = jsonb_set(
+      jsonb_set(
+        jsonb_set(
+          ui_info, -- The corresponding column name of the field to be updated
+        '{APIText}', -- To add text information about the attribute and how it associates to a product
+          to_jsonb((COALESCE(ui_info->>'APIText', '') || 'Page of companies records to return.'))
+      ),
+      '{Products}',
+      (
+        SELECT jsonb_agg(DISTINCT value)
+        FROM jsonb_array_elements_text(
+          COALESCE(ui_info->'Products', '[]'::jsonb) || '["API"]'::jsonb -- The product name, ie API, Roster
+        )
+      )
+    ),
+    '{Company}', -- Set Product associations to the attribute association on a local (endpoint specific) level
+    (
+      SELECT jsonb_agg(DISTINCT value)
+      FROM jsonb_array_elements_text(
+        COALESCE(ui_info->'Company', '[]'::jsonb) || '["API"]'::jsonb -- Endpoint name, Product name, ie API, Roster
+      )
+    )
+  ),
+  associated_endpoints = jsonb_set(
+    associated_endpoints,
+    '{Endpoints}',
+    '["Company"]' -- Set attribute association to the endpoint on a global level
+  ),
+  updated_at = CURRENT_TIMESTAMP
+WHERE attr_title = 'show_paging_totals';
+
+-- Establish Request Type relationship
+UPDATE associated_attrs
+SET
+  request_type = jsonb_set(
+    request_type,
+    '{#Endpoint}',
+    (COALESCE(request_type->'#Endpoint', '[]'::jsonb) || '["Search Request"]'::jsonb)
+  ),
+  updated_at = CURRENT_TIMESTAMP
+WHERE attr_title IN ( -- setting the following associated_attrs -> request_type to contain "#Request Type"
+    'per_page',
+    'page_number',
+    'parent_company_id',
+    'show_paging_totals'
+);
+
+UPDATE associated_attrs
+SET
+  request_type = jsonb_set(
+    request_type,
+    '{#Endpoint}',
+    (COALESCE(request_type->'#Endpoint', '[]'::jsonb) || '["Search Response"]'::jsonb)
+  ),
+  updated_at = CURRENT_TIMESTAMP
+WHERE attr_title IN ( -- setting the following associated_attrs -> request_type to contain "#Request Type"
+    'name',
+    'numeric_id',
+    'client_company_id',
+    'public_partner_attrs'
+);
+
+UPDATE associated_attrs
+SET
+  associated_endpoints = jsonb_set( -- setting the following associated_endpoints to have Brand associations within the Endpoints Array
+    associated_endpoints,
+    '{Endpoints}',
+    (COALESCE(associated_endpoints->'Endpoints', '[]'::jsonb) || '["Company"]'::jsonb)
+  ),
+  updated_at = CURRENT_TIMESTAMP
+WHERE attr_title = 'parent_company_id';
+
+UPDATE associated_attrs
+SET
+  ui_info = jsonb_set(
+    ui_info,
+    '{Company}',
+    (
+      SELECT jsonb_agg(DISTINCT value)
+      FROM jsonb_array_elements_text(
+        COALESCE(ui_info->'Company', '[]'::jsonb) || '["API", "Roster"]'::jsonb
+      )
+    )
+  )
+WHERE attr_title = 'parent_company_id';
+
+END $$;
